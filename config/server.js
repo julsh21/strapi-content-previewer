d8a9d07f9a1a18da5f77f83b8d991b8b93d5767a
Yuliya Sharapa <yuliya.sharapa@bitlogic.io>
2022-01-06 14:36:00 -0300

    base de configuraciones para strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0c619286786a21a71251882681c115bf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0c653f6e-6fb5-467b-82a6-0339dbee3bd2'
+};
