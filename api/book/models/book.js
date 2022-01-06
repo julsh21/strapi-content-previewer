a7873ad3aff18eb438dfd2dbaf69b16d58d3da57
Yuliya Sharapa <yuliya.sharapa@bitlogic.io>
2022-01-06 17:18:50 -0300

    agregando webhook para netlify

--- api/book/models/book.js
+++ api/book/models/book.js
@@ -4,5 +4,35 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
+ const axios = require('axios');
 
-module.exports = {};
+module.exports = {
+// Para que se ejecute cada vez que creamos un articulo nuevo
+afterCreate: async entry => {
+    axios
+      .post(strapi.config.currentEnvironment.staticWebsiteBuildURL, {})
+      .catch(() => {
+        // Ignore
+      });
+  },
+
+// Para que se ejecute cada vez que actualizamos un articulo
+afterUpdate: async entry => {
+    axios
+      .post(strapi.config.currentEnvironment.staticWebsiteBuildURL, {})
+      .catch(() => {
+        // Ignore
+      });
+  },
+
+// Para que se ejecute cada vez que borramos un articulo
+afterDestroy: async entry => {
+    axios
+      .post(strapi.config.currentEnvironment.staticWebsiteBuildURL, {})
+      .catch(() => {
+        // Ignore
+      });
+  },
+
+};
+
---
+++ config/environments/production/custom.js
@@ -0,0 +1,4 @@
+{
+    "myCustomConfiguration": "This configuration is accessible through strapi.config.environments.production.myCustomConfiguration",
+    "staticWebsiteBuildURL": "https://api.netlify.com/build_hooks/61d74c14977a4c90d4193d22"
+}
